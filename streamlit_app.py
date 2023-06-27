0aeb23f17085974b7e92a2d5b36c25b438b2df16
Anton Zaharia <zaharia_anton@yahoo.com>
2023-06-27 19:50:14 +0100

    buttons and conditional rendering

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.write("Hello world!")
+st.header("st.button")
+
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Click"):
+    st.write("Clicked")
+else:
+    st.write("Not clicked")
+
+st.button("Nothing")
